e59aa1f6db344f0830069e645e0d97cfcf8c73df
Daniel Mlynarczyk <Daniel.Mlynarczyk@cshark.com>
2024-05-16 13:27:05 +0200

    Builder instead of configuration and some tests

---
+++ TravelAgency.SharedLibrary.Tests/Vault/VaultFacadeBuilderTests.cs
@@ -0,0 +1,151 @@
+﻿using AutoFixture;
+using Microsoft.Extensions.Hosting;
+using TravelAgency.SharedLibrary.Vault;
+
+namespace TravelAgency.SharedLibrary.Tests.Vault;
+public sealed class VaultFacadeBuilderTests
+{
+    private readonly Fixture _fixture;
+
+    public VaultFacadeBuilderTests()
+    {
+        _fixture = new Fixture();
+    }
+
+    [Fact]
+    public void SetToken_GivenStringIsANull_ThrowsArgumentNullException()
+    {
+        var vaultFacadeBuilder = new VaultFacadeBuilder();
+        string token = null!;
+
+        vaultFacadeBuilder.Invoking(x => x.SetToken(token)).Should().ThrowExactly<ArgumentNullException>();
+    }
+
+    [Fact]
+    public void SetPort_GivenStringIsANull_ThrowsArgumentNullException()
+    {
+        var vaultFacadeBuilder = new VaultFacadeBuilder();
+        string port = null!;
+
+        vaultFacadeBuilder.Invoking(x => x.SetPort(port)).Should().ThrowExactly<ArgumentNullException>();
+    }
+
+    [Fact]
+    public void SetPort_GivenStringCannotBeConvertedToInt_ThrowsArgumentNullException()
+    {
+        var vaultFacadeBuilder = new VaultFacadeBuilder();
+        string port = _fixture.Create<string>();
+
+        vaultFacadeBuilder.Invoking(x => x.SetPort(port)).Should().ThrowExactly<InvalidOperationException>();
+    }
+
+    [Fact]
+    public void SetPort_GivenStringIsZero_ThrowsArgumentNullException()
+    {
+        var vaultFacadeBuilder = new VaultFacadeBuilder();
+        string port = "0";
+
+        vaultFacadeBuilder.Invoking(x => x.SetPort(port)).Should().ThrowExactly<ArgumentException>();
+    }
+
+    [Fact]
+    public void SetPort_GivenIntIsZero_ThrowsArgumentNullException()
+    {
+        var vaultFacadeBuilder = new VaultFacadeBuilder();
+        int port = 0;
+
+        vaultFacadeBuilder.Invoking(x => x.SetPort(port)).Should().ThrowExactly<ArgumentException>();
+    }
+
+    [Fact]
+    public void SetHost_GivenStringIsANull_ThrowsArgumentNullException()
+    {
+        var vaultFacadeBuilder = new VaultFacadeBuilder();
+        string host = null!;
+
+        vaultFacadeBuilder.Invoking(x => x.SetHost(host)).Should().ThrowExactly<ArgumentNullException>();
+    }
+
+    [Fact]
+    public void SetHost_ValidBooleanValue_NotThrow()
+    {
+        var vaultFacadeBuilder = new VaultFacadeBuilder();
+        bool ssl = true;
+
+        vaultFacadeBuilder.Invoking(x => x.SetSSL(ssl)).Should().NotThrow();
+    }
+
+    [Fact]
+    public void SetToken_ValidString_NotThrow()
+    {
+        var vaultFacadeBuilder = new VaultFacadeBuilder();
+        string token = _fixture.Create<string>();
+
+        vaultFacadeBuilder.Invoking(x => x.SetToken(token)).Should().NotThrow();
+    }
+
+    [Fact]
+    public void SetPort_ValidIntAsString_NotThrow()
+    {
+        var vaultFacadeBuilder = new VaultFacadeBuilder();
+        string port = _fixture.Create<int>().ToString();
+
+        vaultFacadeBuilder.Invoking(x => x.SetPort(port)).Should().NotThrow();
+    }
+
+    [Fact]
+    public void SetHost_ValidString_NotThrow()
+    {
+        var vaultFacadeBuilder = new VaultFacadeBuilder();
+        string host = _fixture.Create<string>();
+
+        vaultFacadeBuilder.Invoking(x => x.SetHost(host)).Should().NotThrow();
+    }
+
+    [Fact]
+    public void Build_PortNotSet_ThrowsInvalidOperationException()
+    {
+        var vaultFacadeBuilder = new VaultFacadeBuilder();
+        vaultFacadeBuilder
+            .SetToken(_fixture.Create<string>())
+            .SetHost(_fixture.Create<string>());
+
+        vaultFacadeBuilder.Invoking(x => x.Build()).Should().ThrowExactly<InvalidOperationException>();
+    }
+
+    [Fact]
+    public void Build_TokenNotSet_ThrowsInvalidOperationException()
+    {
+        var vaultFacadeBuilder = new VaultFacadeBuilder();
+        vaultFacadeBuilder
+            .SetPort(_fixture.Create<int>().ToString())
+            .SetHost(_fixture.Create<string>());
+
+        vaultFacadeBuilder.Invoking(x => x.Build()).Should().ThrowExactly<InvalidOperationException>();
+    }
+
+    [Fact]
+    public void Build_HostNotSet_ThrowsInvalidOperationException()
+    {
+        var vaultFacadeBuilder = new VaultFacadeBuilder();
+        vaultFacadeBuilder
+            .SetToken(_fixture.Create<string>())
+            .SetPort(_fixture.Create<int>().ToString());
+
+        vaultFacadeBuilder.Invoking(x => x.Build()).Should().ThrowExactly<InvalidOperationException>();
+    }
+
+    [Fact]
+    public void Build_AllNecesseryParametersSet_ReturnVaultFacade()
+    {
+        var vaultFacadeBuilder = new VaultFacadeBuilder();
+        vaultFacadeBuilder
+            .SetToken(_fixture.Create<string>())
+            .SetPort(_fixture.Create<int>().ToString())
+            .SetHost(_fixture.Create<int>().ToString());
+
+        var facade = vaultFacadeBuilder.Build();
+
+        facade.Should().NotBeNull();    
+    }
+}
---
+++ TravelAgency.SharedLibrary/Vault/VaultFacadeBuilder.cs
@@ -0,0 +1,104 @@
+﻿using VaultSharp;
+using VaultSharp.V1.AuthMethods;
+using VaultSharp.V1.AuthMethods.Token;
+
+namespace TravelAgency.SharedLibrary.Vault;
+public sealed class VaultFacadeBuilder
+{
+    private string? _token;
+    private string? _host;
+    private int? _port;
+    private string _protocol = "http";
+
+    public VaultFacade Build()
+    {
+        if (ValidateIfBuilderIsConfigured() is false)
+        {
+            throw new InvalidOperationException("Builder hasn't been configured correctly");
+        }
+
+        IAuthMethodInfo authMethod = new TokenAuthMethodInfo(_token);
+        var vaultClientSettings = new VaultClientSettings($"{_protocol}://{_host}:{_port}", authMethod);
+        var vaultClient = new VaultClient(vaultClientSettings);
+
+        return new VaultFacade(vaultClient);
+    }
+
+    public VaultFacadeBuilder SetToken(string? token)
+    {
+        if (token is null)
+        {
+            throw new ArgumentNullException($"{nameof(token)}");
+        }
+
+        _token = token;
+
+        return this;
+    }
+
+    public VaultFacadeBuilder SetHost(string? host)
+    {
+        if (host is null)
+        {
+            throw new ArgumentNullException($"{nameof(host)}");
+        }
+
+        _host = host;
+
+        return this;
+    }
+
+    public VaultFacadeBuilder SetPort(string? port)
+    {
+        if (port is null)
+        {
+            throw new ArgumentNullException($"{nameof(port)}");
+        }
+
+        if (int.TryParse(port, out var portAsInt) is false)
+        {
+            throw new InvalidOperationException("Given port cannot be converted to integer");
+        }
+
+        return SetPort(portAsInt);
+    }
+
+    public VaultFacadeBuilder SetPort(int port)
+    {
+        if (port is 0)
+        {
+            throw new ArgumentException($"{nameof(port)} cannot be 0");
+        }
+
+        _port = port;
+
+        return this;
+    }
+
+    public VaultFacadeBuilder SetSSL(bool ssl)
+    {
+        _protocol = ssl ? "https" : "http"; 
+
+        return this;
+    }
+
+    private bool ValidateIfBuilderIsConfigured()
+    {
+        if (_token is null)
+        {
+            return false;
+        }
+
+        if (_host is null)
+        {
+            return false;
+        }
+
+        if (_port is null)
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
